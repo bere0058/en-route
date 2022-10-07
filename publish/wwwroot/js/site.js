3dd6ce9de3c2040f01dee75dffe7638135b0b758
Guillermo Bereche <bere0058@algonquinlive.com>
2022-10-07 16:35:08 -0400

    s

--- publish/wwwroot/js/site.js
+++
@@ -1,6 +0,0 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// Breaking lines
-
-Array.from($("p")).forEach(p => {
-    p.innerHTML = p.textContent.replace("<br>", "<br>")
-});
